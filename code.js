8c04e6d128d3b32ec6f9f359591a9b2c13fbdd1b
Jorden Di Egidio <jorden@uflow.be>
2023-01-12 00:09:41 +0100

    Add page cover + check if cover exists

--- code.js
+++ code.js
@@ -1,14 +1,48 @@
 "use strict";
-figma.showUI(__html__);
-figma.ui.onmessage = (message) => {
-    console.log("message from ui", message);
-    figma.createPage().name = 'Cover';
-    figma.createPage().name = '-------------';
-    figma.createPage().name = 'Design';
-    figma.createPage().name = '⌙ test page';
-    if (message == true) {
-        figma.createPage().name = '-------------';
-        figma.createPage().name = 'archive';
-    }
-    figma.notify('structure added');
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
 };
+figma.showUI(__html__, { width: 400, height: 400, title: "File orangizer" });
+figma.ui.onmessage = (data) => {
+    let archive = data[0];
+    let cover = data[1];
+    let coverTitle = "lorem ipsum";
+    if (cover === true) {
+        createCover(coverTitle);
+    }
+};
+function createCover(title) {
+    //Check if Cover has been made already
+    if (figma.root.children[0].name === "Cover") {
+        //if cover exists -> notify user
+        figma.notify('Cover already created');
+    }
+    else {
+        //if no cover exists, create new page and set name
+        let coverPage = figma.createPage();
+        figma.root.insertChild(0, coverPage);
+        figma.root.children[0].name = "Cover";
+        //navigate to the newly made page
+        figma.currentPage = coverPage;
+        //create cover visual at cover page
+        // TO BE FINALISED!!
+        (() => __awaiter(this, void 0, void 0, function* () {
+            const text = figma.createText();
+            // Move to (50, 50)
+            text.x = 0;
+            text.y = 0;
+            // Load the font in the text node before setting the characters
+            yield figma.loadFontAsync(text.fontName);
+            text.characters = title;
+            // Set bigger font size and red color
+            text.fontSize = 18;
+            text.fills = [{ type: 'SOLID', color: { r: 1, g: 0, b: 0 } }];
+        }))();
+    }
+}
--- code.ts
+++ code.ts
@@ -1,18 +1,46 @@
 
-figma.showUI(__html__);
+figma.showUI(__html__, { width: 400, height: 400, title: "File orangizer" });
 
-figma.ui.onmessage = (message) => {
-  console.log("message from ui", message);
-  
-  figma.createPage().name = 'Cover';
-  figma.createPage().name = '-------------';
-  figma.createPage().name = 'Design';
-  figma.createPage().name = '⌙ test page';
+figma.ui.onmessage = (data) => {
+  let archive = data[0];
+  let cover = data[1];
+  let coverTitle = "lorem ipsum";
 
-  if(message == true) {
-    figma.createPage().name = '-------------';
-    figma.createPage().name = 'archive';
+  if(cover === true){
+   createCover(coverTitle);
   }
+}
 
-  figma.notify('structure added');
+function createCover(title: string){
+  //Check if Cover has been made already
+  if(figma.root.children[0].name === "Cover"){
+    //if cover exists -> notify user
+    figma.notify('Cover already created');
+  } else {
+      //if no cover exists, create new page and set name
+    let coverPage = figma.createPage();
+    figma.root.insertChild(0, coverPage);
+    figma.root.children[0].name = "Cover";
+
+    //navigate to the newly made page
+    figma.currentPage = coverPage;
+
+    //create cover visual at cover page
+    // TO BE FINALISED!!
+    (async () => {
+      const text = figma.createText()
+    
+      // Move to (50, 50)
+      text.x = 0
+      text.y = 0
+    
+      // Load the font in the text node before setting the characters
+      await figma.loadFontAsync(text.fontName)
+      text.characters = title;
+    
+      // Set bigger font size and red color
+      text.fontSize = 18
+      text.fills = [{ type: 'SOLID', color: { r: 1, g: 0, b: 0 } }]
+    })()
+  }
 }
