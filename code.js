64e797610991d6d53aff21f66cfbf5a3ee7d894a
Jorden Di Egidio <jorden@uflow.be>
2023-06-19 11:47:37 +0200

    Set component properties

--- code.js
+++ code.js
@@ -19,49 +19,17 @@ figma.ui.onmessage = (data) => {
     let project = data[6];
     let coverkey = '2c80a7c879f421331226276dfbb9c6b878a99061';
 
-    createCover(coverkey)
-
-    console.log(data);
-    // if (cover === true) {
-    //     createCover(coverTitle);
-    // }
+    createCover(coverkey, coverTitle, project);
 };
 
-function createCover(coverkey) {
-    importNode(coverkey);
-    
+async function createCover(coverkey, coverTitle, project) {
+    let importComponent = await figma.importComponentByKeyAsync(coverkey);
+    let instance = importComponent.createInstance();
 
-    //Check if Cover has been made already
-    // if (figma.root.children[0].name === "Cover") {
-    //     //if cover exists -> notify user
-    //     figma.notify('Cover already created');
-    // }
-    // else {
-    //     //if no cover exists, create new page and set name
-    //     let coverPage = figma.createPage();
-    //     figma.root.insertChild(0, coverPage);
-    //     figma.root.children[0].name = "Cover";
-    //     //navigate to the newly made page
-    //     figma.currentPage = coverPage;
-    //     //create cover visual at cover page
-    //     // TO BE FINALISED!!
-    //     (() => __awaiter(this, void 0, void 0, function* () {
-    //         const text = figma.createText();
-    //         // Move to (50, 50)
-    //         text.x = 0;
-    //         text.y = 0;
-    //         // Load the font in the text node before setting the characters
-    //         yield figma.loadFontAsync(text.fontName);
-    //         text.characters = title;
-    //         // Set bigger font size and red color
-    //         text.fontSize = 18;
-    //         text.fills = [{ type: 'SOLID', color: { r: 1, g: 0, b: 0 } }];
-    //     }))();
-    // }
-}
+    instance.setProperties({
+        'title#10694:0' : coverTitle,
+        'Project#10718:5': project
+    });
 
-async function importNode(key) {
-    let importComponent = await figma.importComponentByKeyAsync(key);
-    importComponent.createInstance();
 }
 
